0855e74ae2505f5628345e130fced1c7583b00c9
Grupo EGS <grupoegs_tecnologia@Grupos-MacBook-Air.local>
2026-05-23 18:34:53 -0500

    feat: se agrego la funcionalidad de completar tareas, o cambiarlas en estado atrasada en cambio de que se haya pasado la fecha limite

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <h1>feature/agregar-tareas</h1>
+    <>
+      <h1>feature/agregar-tareas</h1>
+      <h1>feature/completar-tareas</h1>
+    </>
+
   )
 }
 
